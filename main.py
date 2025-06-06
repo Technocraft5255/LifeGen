812f287ce00be678ead1da7bd04aec9773d2fb53
Technocraft5255 <emilien.mariau@gmail.com>
2025-06-06 23:33:18 +0200

    Minor changes and fixes

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ class Window:
 
 
 
-            with dpg.drawlist(width=700, height=700):  # drawlist (,callback=self.test)
+            with dpg.drawlist(width=700, height=700,callback=self.test):  # drawlist ()
                 for i in range(10):
                     for j in range(10):
                         dpg.draw_rectangle((10 + 50 * i, 10 + 50 * j), (50 + 50 * i, 50 + 50 * j),
@@ -45,9 +45,9 @@ class Window:
                 dpg.draw_rectangle((60, 60), (100, 100), fill=(255, 0, 0), color=(255, 0, 0), thickness=0.0)
 
 
-    """def test(self, sender, app_data, user_data):
+    def test(self, sender, app_data, user_data):
         print(sender, app_data, user_data)
-        print(dpg.get_mouse_pos())"""
+        print(dpg.get_mouse_pos())
 
 
 window = Window()
