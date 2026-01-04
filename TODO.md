27918d32798b2a29bd7c32104812faabbdeac490
Technocraft5255 <88849811+Technocraft5255@users.noreply.github.com>
2026-01-04 22:43:27 +0100

    Added chunk and cell adding methods in Game class Added viewport cell rendering and temporary cell showing in the viewport

--- TODO.md
+++ TODO.md
@@ -1,6 +1,6 @@
 ## Features to deploy in LifeGen
 - [x] Create simple gui with key and mouse actions handling
-- [ ] Create viewport widget that supports dragging and zooming
+- [x] Create viewport widget that supports dragging and zooming
 - [ ] Create a conway's life simulation
 - [ ] Allow sim editing in viewport
 - [ ] Allow sim launching
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+from math import floor
 import pygame as pg
 from pygame import Vector2
 
 
 class Game:
     def __init__(self):
-        pass
+        # Chunk map stocks : {chunk_pos (Vector2): chunk (list of Vector2)}
+        self.chunk_map = {}
 
     def open(self, file_path):
         # Load saved life simulation file here
@@ -14,9 +16,20 @@ class Game:
         # Save current simulation state in a file
         pass
 
+    def add_cell(self, cell):
+        cell_chunk = self.get_chunk(cell)
+        cell_chunk.append(cell)
+
+    def get_chunk(self, cell: Vector2) -> Vector2:
+        chunk_pos = Vector2(floor(cell[0] / 16), floor(cell[1] / 16))
+        if chunk_pos not in self.chunk_map:
+            self.chunk_map[chunk_pos] = []
+        return self.chunk_map[chunk_pos]
+
 
 class App:
     def __init__(self, game_instance: Game):
+        self.temp = []
         pg.init()
         self.fps = 60
         self.clock = pg.time.Clock()
@@ -25,24 +38,36 @@ class App:
 
         # Window size and creation
         self.size = Vector2(1000, 800)
+        self.SSAA_factor = 2
+
+        self.SSAA_surface = pg.Surface(self.size * self.SSAA_factor)
         self.screen = pg.display.set_mode(self.size, pg.RESIZABLE)
 
         # Viewport creation
         self.viewport = ViewPort(Vector2(0, 50), self.size - Vector2(0, 50))
 
         self.last_mouse_pos = Vector2(pg.mouse.get_pos())
+        self.drag_delta = 0  # To detect if a click is a drag or a simple click
 
     def run(self):
         """Main application loop."""
         while self.is_running:
-            self.screen.fill((0, 0, 0))
+            self.SSAA_surface.fill((0, 0, 0))
 
             # Draw viewport area and test rectangle
-            self.viewport.draw(self.screen)
-            self.viewport.draw_rect(self.screen, Vector2(2, 2), Vector2(3, 3))
+            self.viewport.draw(self.SSAA_surface, (50, 50, 50))
+            for row in range(int(self.viewport.start_pos.x)-5, int(self.viewport.end_pos.x)+5, 1):
+                for col in range(int(self.viewport.start_pos.y)-5, int(self.viewport.end_pos.y)+5, 1):
+                    self.viewport.draw_rect(self.SSAA_surface, (0, 0, 0), Vector2(row + 0.05, col + 0.05),
+                                            Vector2(row + 1, col + 1))
+            for cell in self.temp:
+                self.viewport.draw_rect(self.SSAA_surface, (255, 255, 255), cell, cell+Vector2(1, 1))
 
             # Limit FPS and update screen
             self.clock.tick(self.fps)
+            self.screen.blit(pg.transform.smoothscale(
+                self.SSAA_surface, self.size*self.SSAA_factor
+            ), (0, 0))
             pg.display.flip()
 
             # Process incoming events
@@ -59,13 +84,21 @@ class App:
             if event.type == pg.MOUSEBUTTONDOWN:
                 self.viewport.dragging = True
                 self.last_mouse_pos = Vector2(pg.mouse.get_pos())
+                self.drag_delta = 0
 
             if event.type == pg.MOUSEBUTTONUP:
                 self.viewport.dragging = False
+                if self.drag_delta < 2 and event.button == 1:
+                    val = self.viewport.screen_to_viewport(Vector2(pg.mouse.get_pos()))
+                    val.x = floor(val.x)
+                    val.y = floor(val.y)
+                    self.temp.append(val)
+                    print("val", val)
 
             if event.type == pg.MOUSEMOTION:
                 if self.viewport.dragging:
                     self.viewport.handle_drag(Vector2(pg.mouse.get_pos()), self.last_mouse_pos)
+                    self.drag_delta += (self.last_mouse_pos - Vector2(pg.mouse.get_pos())).length()
                     self.last_mouse_pos = Vector2(pg.mouse.get_pos())
 
             # Zoom in/out with mouse wheel
@@ -88,7 +121,7 @@ class ViewPort:
 
         self.dragging = False
 
-    def viewport_to_screen(self, pos: Vector2):
+    def viewport_to_screen(self, pos: Vector2) -> Vector2:
         """
         Convert a coordinate in world-space to a pixel coordinate on screen.
         Performs linear interpolation from world-space to screen-space.
@@ -100,7 +133,7 @@ class ViewPort:
             (self.end_pos.y - self.start_pos.y) * self.viewport_size.y + self.viewport_pos.y
         )
 
-    def screen_to_viewport(self, pos: Vector2):
+    def screen_to_viewport(self, pos: Vector2) -> Vector2:
         """
         Convert a pixel coordinate on screen to world-space coordinates.
         """
@@ -114,9 +147,9 @@ class ViewPort:
             (self.end_pos.y - self.start_pos.y) * relative_pos.y + self.start_pos.y,
         )
 
-    def draw(self, screen):
+    def draw(self, screen, background_color):
         """Draw the viewport boundary (simple white rectangle)."""
-        pg.draw.rect(screen, (255, 255, 255), (self.viewport_pos, self.viewport_size))
+        pg.draw.rect(screen, background_color, (self.viewport_pos, self.viewport_size))
 
     def handle_zoom(self, mouse_pos: Vector2, zoom_value: int | float):
         """
@@ -137,10 +170,10 @@ class ViewPort:
         # Vectors from mouse to viewport boundaries
         to_start_pos_vector = self.start_pos - viewport_mouse_pos
         to_end_pos_vector = self.end_pos - viewport_mouse_pos
-
-        # Apply zoom by scaling these vectors
-        self.start_pos += to_start_pos_vector * zoom_value
-        self.end_pos += to_end_pos_vector * zoom_value
+        # Apply zoom by scaling these vectors if zoom is not too high or too low
+        if ((self.end_pos-self.start_pos).length() < 500 and zoom_value >= 0) or ((self.end_pos-self.start_pos).length() > 1 and zoom_value < 0):
+            self.start_pos += to_start_pos_vector * zoom_value
+            self.end_pos += to_end_pos_vector * zoom_value
 
         # Cleanup temporary objects
         del to_start_pos_vector, to_end_pos_vector, viewport_mouse_pos
@@ -153,11 +186,11 @@ class ViewPort:
         self.start_pos += world_last_mouse_pos - world_mouse_pos
         self.end_pos += world_last_mouse_pos - world_mouse_pos
 
-    def draw_rect(self, screen, pos1, pos2):
+    def draw_rect(self, screen, color, pos1, pos2):
         """Draw a red rectangle using world coordinates."""
         screen_pos1 = self.viewport_to_screen(pos1)
         screen_pos2 = self.viewport_to_screen(pos2)
-        pg.draw.rect(screen, (255, 0, 0),
+        pg.draw.rect(screen, color,
                      (screen_pos1, screen_pos2 - screen_pos1))
 
 
